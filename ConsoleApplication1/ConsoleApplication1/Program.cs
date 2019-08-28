5373a41fd65f5a3fa0de53f1996a902f8e3d1cd9
wisdomcd <wisdomcdh@gmail.com>
2019-08-28 13:05:10 +0900

    ff

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //123
+            //1231
             // 1
             // 2
             // hotfix
